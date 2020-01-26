6b8c06a377d077e6523e0ba32af7833f2c51e4e1
akshay <akshay.dr.11@gmail.com>
2020-01-26 19:49:32 +0100

    Added Main function gym environment

--- ddpg.py
+++ ddpg.py
@@ -1,13 +1,8 @@
 import tensorflow as tf
 import numpy as np
-import gym
-from gym import wrappers
-import argparse
-import pprint as pp
 
 from replayBuffer import ReplayBuffer
-from actorCritic import ActorNetwork, CriticNetwork
-from utilities import OrnsteinUhlenbeckActionNoise, build_summaries
+from utilities import build_summaries
 
 #   Agent Training
 def train(sess, env, args, actor, critic, actor_noise):
@@ -93,45 +88,3 @@ def train(sess, env, args, actor, critic, actor_noise):
                 print('| Reward: {:d} | Episode: {:d} | Qmax: {:.4f}'.format(int(ep_reward), \
                         i, (ep_ave_max_q / float(j))))
                 break
-
-def main(args):
-    with tf.compat.v1.Session() as sess:
-
-        env = gym.make(args['env'])
-        np.random.seed(int(args['random_seed']))
-        tf.compat.v1.set_random_seed(int(args['random_seed']))
-        env.seed(int(args['random_seed']))
-
-        state_dim = env.observation_space.shape[0]
-        action_dim = env.action_space.shape[0]
-        action_bound = env.action_space.high
-        
-        # Ensure action bound is symmetric
-        try:
-            assert (env.action_space.high == -env.action_space.low)
-        except:
-            ValueError
-
-        actor = ActorNetwork(sess, state_dim, action_dim, action_bound,
-                             float(args['actor_lr']), float(args['tau']),
-                             int(args['minibatch_size']))
-
-        critic = CriticNetwork(sess, state_dim, action_dim,
-                               float(args['critic_lr']), float(args['tau']),
-                               float(args['gamma']),
-                               actor.get_num_trainable_vars())
-        
-        actor_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(action_dim))
-
-        if args['use_gym_monitor']:
-            if not args['render_env']:
-                env = wrappers.Monitor(
-                    env, args['monitor_dir'], video_callable=False, force=True)
-            else:
-                env = wrappers.Monitor(env, args['monitor_dir'], force=True)
-
-        # Train the model
-        train(sess, env, args, actor, critic, actor_noise)
-
-        if args['use_gym_monitor']:
-            env.monitor.close()
---
+++ main.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+import numpy as np
+import gym
+from gym import wrappers
+import argparse
+import pprint as pp
+
+from actorCritic import ActorNetwork, CriticNetwork
+from utilities import OrnsteinUhlenbeckActionNoise
+from ddpg import train
+
+def main(args):
+
+    with tf.compat.v1.Session() as sess:
+
+        env = gym.make(args['env'])
+        np.random.seed(int(args['random_seed']))
+        tf.compat.v1.set_random_seed(int(args['random_seed']))
+        env.seed(int(args['random_seed']))
+
+        state_dim = env.observation_space.shape[0]
+        action_dim = env.action_space.shape[0]
+        action_bound = env.action_space.high
+        
+        try:
+        # Ensure action bound is symmetric
+            assert (env.action_space.high == -env.action_space.low)
+        except:
+            ValueError
+
+        actor = ActorNetwork(sess, state_dim, action_dim, action_bound,
+                             float(args['actor_lr']), float(args['tau']),
+                             int(args['minibatch_size']))
+
+        critic = CriticNetwork(sess, state_dim, action_dim,
+                               float(args['critic_lr']), float(args['tau']),
+                               float(args['gamma']),
+                               actor.get_num_trainable_vars())
+        
+        actor_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(action_dim))
+
+        if args['use_gym_monitor']:
+            if not args['render_env']:
+                env = wrappers.Monitor(
+                    env, args['monitor_dir'], video_callable=False, force=True)
+            else:
+                env = wrappers.Monitor(env, args['monitor_dir'], force=True)
+
+        train(sess, env, args, actor, critic, actor_noise)
+
+        if args['use_gym_monitor']:
+            env.monitor.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='provide arguments for DDPG agent')
+
+    # agent parameters
+    parser.add_argument('--actor-lr', help='actor network learning rate', default=0.0001)
+    parser.add_argument('--critic-lr', help='critic network learning rate', default=0.001)
+    parser.add_argument('--gamma', help='discount factor for critic updates', default=0.75)
+    parser.add_argument('--tau', help='soft target update parameter', default=0.01)
+    parser.add_argument('--buffer-size', help='max size of the replay buffer', default=1000000)
+    parser.add_argument('--minibatch-size', help='size of minibatch for minibatch-SGD', default=64)
+
+    # MountainCarContinuous-v0
+    # Ant-v2
+    # run parameters
+    parser.add_argument('--env', help='choose the gym env- tested on {Pendulum-v0}', default='Ant-v2')
+    parser.add_argument('--random-seed', help='random seed for repeatability', default=1108)
+    parser.add_argument('--max-episodes', help='max num of episodes to do while training', default=50000)
+    parser.add_argument('--max-episode-len', help='max length of 1 episode', default=1000)
+    parser.add_argument('--render-env', help='render the gym env', action='store_true')
+    parser.add_argument('--use-gym-monitor', help='record gym results', action='store_true')
+    parser.add_argument('--monitor-dir', help='directory for storing gym results', default='./results/gym_ddpg')
+    parser.add_argument('--summary-dir', help='directory for storing tensorboard info', default='./results/tf_ddpg')
+
+    parser.set_defaults(render_env=True)
+    parser.set_defaults(use_gym_monitor=True)
+    
+    args = vars(parser.parse_args())
+    
+    pp.pprint(args)
+
+    main(args)
