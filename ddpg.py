d2850f92f65e9d0c3b4515812348f842b64b7f35
akshay <akshay.dr.11@gmail.com>
2020-01-26 19:42:36 +0100

    Added DDPG Algorithm code and noise function

---
+++ ddpg.py
@@ -0,0 +1,137 @@
+import tensorflow as tf
+import numpy as np
+import gym
+from gym import wrappers
+import argparse
+import pprint as pp
+
+from replayBuffer import ReplayBuffer
+from actorCritic import ActorNetwork, CriticNetwork
+from utilities import OrnsteinUhlenbeckActionNoise, build_summaries
+
+#   Agent Training
+def train(sess, env, args, actor, critic, actor_noise):
+
+    # Setup Summary
+    summary_ops, summary_vars = build_summaries()
+
+    sess.run(tf.compat.v1.global_variables_initializer())
+    writer = tf.compat.v1.summary.FileWriter(args['summary_dir'], sess.graph)
+
+    # Initialize target network weights
+    actor.update_target_network()
+    critic.update_target_network()
+
+    # Initialize replay memory
+    replay_buffer = ReplayBuffer(int(args['buffer_size']), int(args['random_seed']))
+
+    for i in range(int(args['max_episodes'])):
+
+        s = env.reset()
+        ep_reward = 0
+        ep_ave_max_q = 0
+
+        for j in range(int(args['max_episode_len'])):
+
+            if args['render_env']:
+                env.render()
+
+            # Add exploration noise
+            a = actor.predict(np.reshape(s, (1, actor.s_dim))) + actor_noise()
+
+            s2, r, terminal, info = env.step(a[0])
+
+            replay_buffer.add(np.reshape(s, (actor.s_dim,)), np.reshape(a, (actor.a_dim,)), r,
+                              terminal, np.reshape(s2, (actor.s_dim,)))
+
+            # Keep adding experience to the memory until
+            # there are at least minibatch size samples
+            if replay_buffer.size() > int(args['minibatch_size']):
+                s_batch, a_batch, r_batch, t_batch, s2_batch = \
+                    replay_buffer.sample_batch(int(args['minibatch_size']))
+
+                # Calculate targets
+                target_q = critic.predict_target(
+                    s2_batch, actor.predict_target(s2_batch))
+
+                y_i = []
+                for k in range(int(args['minibatch_size'])):
+                    if t_batch[k]:
+                        y_i.append(r_batch[k])
+                    else:
+                        y_i.append(r_batch[k] + critic.gamma * target_q[k])
+
+                # Update the critic given the targets
+                predicted_q_value, _ = critic.train(
+                    s_batch, a_batch, np.reshape(y_i, (int(args['minibatch_size']), 1)))
+
+                # Find argmax q value of the current episode
+                ep_ave_max_q += np.amax(predicted_q_value)
+
+                # Update the actor policy using the sampled gradient
+                a_outs = actor.predict(s_batch)
+                grads = critic.action_gradients(s_batch, a_outs)
+                actor.train(s_batch, grads[0])
+
+                # Update target networks
+                actor.update_target_network()
+                critic.update_target_network()
+
+            s = s2
+            ep_reward += r
+
+            if terminal:
+                if (summary_ops != None):
+                    summary_str = sess.run(summary_ops, feed_dict={
+                        summary_vars[0]: ep_reward,
+                        summary_vars[1]: ep_ave_max_q / float(j)
+                    })
+
+                    writer.add_summary(summary_str, i)
+                    writer.flush()
+
+                print('| Reward: {:d} | Episode: {:d} | Qmax: {:.4f}'.format(int(ep_reward), \
+                        i, (ep_ave_max_q / float(j))))
+                break
+
+def main(args):
+    with tf.compat.v1.Session() as sess:
+
+        env = gym.make(args['env'])
+        np.random.seed(int(args['random_seed']))
+        tf.compat.v1.set_random_seed(int(args['random_seed']))
+        env.seed(int(args['random_seed']))
+
+        state_dim = env.observation_space.shape[0]
+        action_dim = env.action_space.shape[0]
+        action_bound = env.action_space.high
+        
+        # Ensure action bound is symmetric
+        try:
+            assert (env.action_space.high == -env.action_space.low)
+        except:
+            ValueError
+
+        actor = ActorNetwork(sess, state_dim, action_dim, action_bound,
+                             float(args['actor_lr']), float(args['tau']),
+                             int(args['minibatch_size']))
+
+        critic = CriticNetwork(sess, state_dim, action_dim,
+                               float(args['critic_lr']), float(args['tau']),
+                               float(args['gamma']),
+                               actor.get_num_trainable_vars())
+        
+        actor_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(action_dim))
+
+        if args['use_gym_monitor']:
+            if not args['render_env']:
+                env = wrappers.Monitor(
+                    env, args['monitor_dir'], video_callable=False, force=True)
+            else:
+                env = wrappers.Monitor(env, args['monitor_dir'], force=True)
+
+        # Train the model
+        train(sess, env, args, actor, critic, actor_noise)
+
+        if args['use_gym_monitor']:
+            env.monitor.close()
---
+++ utilities.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+import numpy as np
+
+# Taken from https://github.com/openai/baselines/blob/master/baselines/ddpg/noise.py, which is
+# based on http://math.stackexchange.com/questions/1287634/implementing-ornstein-uhlenbeck-in-matlab
+class OrnsteinUhlenbeckActionNoise:
+    def __init__(self, mu, sigma=0.3, theta=.15, dt=1e-2, x0=None):
+        self.theta = theta
+        self.mu = mu
+        self.sigma = sigma
+        self.dt = dt
+        self.x0 = x0
+        self.reset()
+
+    def __call__(self):
+        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
+                self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
+        self.x_prev = x
+        return x
+
+    def reset(self):
+        self.x_prev = self.x0 if self.x0 is not None else np.zeros_like(self.mu)
+
+    def __repr__(self):
+        return 'OrnsteinUhlenbeckActionNoise(mu={}, sigma={})'.format(self.mu, self.sigma)
+
+#   Tensorflow Summary Ops
+def build_summaries():
+    episode_reward = tf.Variable(0.)
+    tf.summary.scalar("Reward", episode_reward)
+    episode_ave_max_q = tf.Variable(0.)
+    tf.summary.scalar("Qmax Value", episode_ave_max_q)
+
+    summary_vars = [episode_reward, episode_ave_max_q]
+    summary_ops = tf.compat.v1.summary.merge_all()
+    print(summary_ops)
+    return summary_ops, summary_vars
+
